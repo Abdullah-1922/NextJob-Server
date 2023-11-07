a50b0d10f07a3cff019fb3b645c611205b94bff7
Abdullah-1922 <kafikafi1922@gmail.com>
2023-11-08 02:06:04 +0600

    job add and update complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  const cors = require('cors');
  const app = express();
  const port =process.env.PORT || 5000;
- const { MongoClient, ServerApiVersion } = require('mongodb');
+ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
  require('dotenv').config()
  app.use(cors());
  app.use(express.json());
@@ -18,6 +18,8 @@
      deprecationErrors: true,
    }
  });
+const jobCollection =client.db("nextJob").collection("allJobs")
+
  async function run() {
    try {
      
@@ -32,8 +34,68 @@
  }
  run().catch(console.dir);
 
+app.post('/alljobs',async(req,res)=>{
+  const job = req.body;
+  try{
+    const result =await jobCollection.insertOne(job);
+    res.send(result);
+    
 
+  }catch(err){
+    return {message:err.message ,err:true}
+  }
+  
+})
 
+app.get('/myJobs', async(req,res)=>{
+
+  const query = {email: req.query.email}
+   
+  try{
+    const result =await jobCollection.find(query).toArray();
+    res.send(result);
+  }catch(err){
+    return {message:err.message ,err:true}
+  }
+  
+})
+app.delete('/alljobs/:id',async(req,res)=>{
+  const id =req.params.id;
+   const query = {_id:new ObjectId(id)}
+   try{
+    const result =await jobCollection.deleteOne(query);
+    res.send(result);
+   }catch(err){
+    return {message:err.message ,err:true}
+   }
+})
+app.get('/updatejob/:id',async(req,res)=>{
+const id =req.params.id;
+const query = {_id:new ObjectId(id)}
+try{
+  const result =await jobCollection.findOne(query);
+  res.send(result);
+}catch(err){
+  return {message:err.message ,err:true}
+}
+
+})
+app.put('/updatejob/:id',async(req,res)=>{
+  const id =req.params.id;
+  const job = req.body;
+  const filter = {_id:new ObjectId(id)}
+  const options={upsert:true}
+  const updatedJob ={
+    $set:job
+  }
+  try{
+    const result =await jobCollection.updateOne(filter,updatedJob,options);
+    res.send(result);
+   
+  }catch(err){
+    return {message:err.message ,err:true}
+  }
+})
  app.get('/',(req,res)=>{
     res.send('server is running')
  })
